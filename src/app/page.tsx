fed8da350dba68522e9837d8ea78e9975fa6818c
DigitalMatchGlobal <digitalmatch.global@gmail.com>
2025-12-27 21:19:33 -0300

    page++

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+    import LandingPageInteractive from './landing-page/components/LandingPageInteractive';
+
+    export const metadata = {
+    title: 'Victor Cuellar | Preparación Física',
+    description: 'Entrenamiento de alto rendimiento y fortaleza mental en Salta.',
+    };
+
+    export default function Home() {
+    return (
+        <main>
+        <LandingPageInteractive />
+        </main>
+    );
+    }
