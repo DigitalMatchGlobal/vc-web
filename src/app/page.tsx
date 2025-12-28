60fbf22c8213af84392b0cf8010c49e9bdf74751
DigitalMatchGlobal <digitalmatch.global@gmail.com>
2025-12-27 21:24:12 -0300

    page++

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
     import LandingPageInteractive from './landing-page/components/LandingPageInteractive';
 
     export const metadata = {
-    title: 'Victor Cuellar | Preparación Física',
-    description: 'Entrenamiento de alto rendimiento y fortaleza mental en Salta.',
+    title: 'Victor Cuellar - Preparación Física Integral en Salta',
+    description: 'Sistema integral de rendimiento deportivo con +25 años de experiencia. Entrenamiento personalizado, coaching mental y nutrición.',
     };
 
     export default function Home() {
